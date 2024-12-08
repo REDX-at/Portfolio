6b7fd10b565206b6dec934c378f0f8d302be8614
Aimen Taoussi <aitaouss@e2r8p10.1337.ma>
2024-12-08 22:05:01 +0100

    Utils and Pictures Added

--- portfolio/src/component/AboutMe.jsx
+++ portfolio/src/component/AboutMe.jsx
@@ -1,11 +1,13 @@
-import Icon from "../utils/Utils";
+import Icon, { Colors } from "../utils/Utils";
+import Man1 from "../imgs/Man1.png";
+import Man2 from "../imgs/Man2.png";
 
 export default function AboutMe() {
   return (
     <div>
       {" "}
       <div className="grid grid-cols-6">
-        <div className="h-screen col-span-4 text-center pt-[5%]">
+        <div className="h-screen col-span-4 text-center flex flex-col items-center justify-center">
           <div className="flex flex-col items-center justify-center gap-16">
             <div>
               <span className="text-[280px] font-bold ">About Me</span>
@@ -13,15 +15,21 @@ export default function AboutMe() {
             <div className="w-[60%] ">
               <p className="text-4xl leading-relaxed">
                 I am a professional{" "}
-                <span className="bg-red-600 text-white rounded-full py-1 px-4">
+                <span
+                  className={`bg-${Colors.accent600} text-white rounded-full py-1 px-4`}
+                >
                   Graphic Designer
                 </span>{" "}
                 ,{" "}
-                <span className="bg-red-600 text-white rounded-full py-1 px-4">
+                <span
+                  className={`bg-${Colors.accent600} text-white rounded-full py-1 px-4`}
+                >
                   UI/UX Designer
                 </span>
                 , and{" "}
-                <span className="bg-red-600 text-white rounded-full py-1 px-4">
+                <span
+                  className={`bg-${Colors.accent600} text-white rounded-full py-1 px-4`}
+                >
                   Front-End Developer
                 </span>{" "}
                 with a passion for creating visually compelling designs and
@@ -31,10 +39,10 @@ export default function AboutMe() {
                 exceptional results.
               </p>
             </div>
-            <div className="bg-black py-6 px-10 rounded-full mt-11">
+            <div className="bg-black py-6 px-10 rounded-full mt-11 cursor-pointer">
               <div className="flex items-center gap-6">
                 <button className="text-2xl text-white font-bold">
-                  <a href="https://github.com/REDX-at">Go check</a>
+                  <p>Go check</p>
                 </button>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -47,13 +55,13 @@ export default function AboutMe() {
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                    d="m19.5 8.25-7.5 7.5-7.5-7.5"
                   />
                 </svg>
               </div>
             </div>
           </div>
-          <div className="flex items-center justify-center gap-10 mt-4">
+          <div className="flex items-center justify-center gap-16 mt-4">
             <div>
               <a href="https://www.fiverr.com/taaimen">{Icon.fiver}</a>
             </div>
@@ -62,8 +70,13 @@ export default function AboutMe() {
             </div>
           </div>
         </div>
-        <div className="h-screen bg-black rounded-l-[100px] col-span-2 ">
-          <h1 className="text-white  text-[200px] w-[100%] leading-tight font-bold pl-3">
+        <div className="h-screen rounded-l-[100px] col-span-2  overflow-hidden relative">
+          <img
+            src={Man2}
+            alt="Aimen Taoussi"
+            className="w-full h-full object-cover"
+          />
+          <h1 className="text-white  text-[200px] w-[100%] leading-[12rem] font-bold pl-3 absolute top-0 left-0 z-1">
             AIMEN TAOUSSI
           </h1>
         </div>
--- portfolio/src/component/Header.jsx
+++ portfolio/src/component/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Icon, { Colors } from "../utils/Utils";
 
 export default function Header() {
   const arrHeader = ["Home", "About me", "Product", "Contact Me"];
@@ -11,11 +12,15 @@ export default function Header() {
     <div className="h-20 grid grid-cols-5 gap-2 mt-10 text-center fixed top-0 left-0 w-full z-10">
       <span className="self-center justify-self-center font-bold text-xl text-white bg-black py-6 rounded-full w-80">
         Designed By{" "}
-        <span className="bg-red-600 text-white px-4 rounded-full py-1">
+        <span
+          className={`bg-${Colors.accent600} text-white px-4 rounded-full py-1`}
+        >
           Redx
         </span>
       </span>
-      <div className="text-center bg-red-600 col-span-3 rounded-full grid grid-cols-4 items-center text-white font-bold">
+      <div
+        className={`text-center bg-${Colors.accent600} col-span-3 rounded-full grid grid-cols-4 items-center text-white font-bold`}
+      >
         {arrHeader.map((item) => (
           <span
             key={item}
@@ -34,22 +39,9 @@ export default function Header() {
         ))}
       </div>
       <button className="bg-black w-52 mx-auto rounded-full text-white">
-        <div className="flex justify-center gap-6 font-bold ">
+        <div className="flex justify-center gap-6 font-bold items-center">
           <span>Hire me</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-            />
-          </svg>
+          {Icon.download}
         </div>
       </button>
     </div>
